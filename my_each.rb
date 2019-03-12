bd9a16a54f46ae9f9207c835662022efb1db01f5
Kate F <follett.katie@gmail.com>
2019-03-12 16:24:45 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,9 @@
-require 'pry'
-def my_each (array)
+def my_each (words)
   i = 0
-   while i < array
-array do |word|
-    end
+  while i < words.length
+    yield(words[i])
+    i+=1
+
   end
+  words
 end
